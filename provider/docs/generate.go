be3850625d2ab362b25d4f50819f161cfce57baf
Tommy <97602472+tommythe@users.noreply.github.com>
2022-12-21 21:35:08 +0100

    Move swagger to separate package to avoid bloating downstream dependants

--- provider/docs/generate.go
+++ provider/docs/generate.go
@@ -1,4 +1,4 @@
 package docs
 
 // Run using "go generate ./..."
-//go:generate go run github.com/swaggo/swag/cmd/swag@latest init -g ./ops/swagger.go -d ../../ -o ./generated/ --instanceName provider --ot go,yaml
+//go:generate go run github.com/swaggo/swag/cmd/swag@latest init -g ./swagger/swagger.go -d ../../ -o ./generated/ --instanceName provider --ot go,yaml
