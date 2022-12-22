df464fad59ce6eefab1d31292cd46ed969e76447
Gustav Stensson <103995817+gustavste@users.noreply.github.com>
2022-12-22 11:08:11 +0100

    revert convert to oapi3. Do that where needed instead

--- provider/docs/generate.go
+++ provider/docs/generate.go
@@ -2,4 +2,3 @@ package docs
 
 // Run using "go generate ./..."
 //go:generate go run github.com/swaggo/swag/cmd/swag@latest init -g ./ops/swagger.go -d ../../ -o ./generated/ --instanceName provider --ot go,yaml
-//go:generate yarn dlx swagger2openapi -o ./generated/provider_swagger.yaml ./generated/provider_swagger.yaml
