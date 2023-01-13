976e24bf5507af475f3a0bb5283bd7f6a160f505
Tommy <97602472+tommythe@users.noreply.github.com>
2023-01-13 14:35:26 +0100

    Decouple Caleta mapper and service instance

--- provider/caleta/model_mapper.go
+++ provider/caleta/model_mapper.go
@@ -9,7 +9,7 @@ import (
 	"github.com/valkyrie-fnd/valkyrie/pam"
 )
 
-func (s *ProviderService) balanceRequestMapper(ctx context.Context, r WalletbalanceJSONRequestBody) pam.GetBalanceRequestMapper {
+func balanceRequestMapper(ctx context.Context, r WalletbalanceJSONRequestBody) pam.GetBalanceRequestMapper {
 	return func() (context.Context, pam.GetBalanceRequest, error) {
 		return ctx, pam.GetBalanceRequest{
 			PlayerID: r.SupplierUser,
@@ -22,7 +22,7 @@ func (s *ProviderService) balanceRequestMapper(ctx context.Context, r Walletbala
 	}
 }
 
-func (s *ProviderService) getSessionMapper(ctx context.Context, token, requestID string) pam.GetSessionRequestMapper {
+func getSessionMapper(ctx context.Context, token, requestID string) pam.GetSessionRequestMapper {
 	return func() (context.Context, pam.GetSessionRequest, error) {
 		return ctx, pam.GetSessionRequest{
 			Params: pam.GetSessionParams{
@@ -34,7 +34,7 @@ func (s *ProviderService) getSessionMapper(ctx context.Context, token, requestID
 	}
 }
 
-func (s *ProviderService) refreshSessionMapper(ctx context.Context, token, requestID string) pam.RefreshSessionRequestMapper {
+func refreshSessionMapper(ctx context.Context, token, requestID string) pam.RefreshSessionRequestMapper {
 	return func() (context.Context, pam.RefreshSessionRequest, error) {
 		return ctx, pam.RefreshSessionRequest{
 			Params: pam.RefreshSessionParams{
@@ -46,8 +46,8 @@ func (s *ProviderService) refreshSessionMapper(ctx context.Context, token, reque
 	}
 }
 
-func (s *ProviderService) betTransactionMapper(ctx context.Context, r WalletbetJSONRequestBody) pam.AddTransactionRequestMapper {
-	return func(ar pam.AmountRounder) (context.Context, *pam.AddTransactionRequest, error) {
+func betTransactionMapper(ctx context.Context, r WalletbetJSONRequestBody) pam.AddTransactionRequestMapper {
+	return func(_ pam.AmountRounder) (context.Context, *pam.AddTransactionRequest, error) {
 		amt := toPamAmount(&r.Amount)
 		return ctx, &pam.AddTransactionRequest{
 			PlayerID: r.SupplierUser,
@@ -71,8 +71,8 @@ func (s *ProviderService) betTransactionMapper(ctx context.Context, r WalletbetJ
 		}, nil
 	}
 }
-func (s *ProviderService) promoBetTransactionMapper(ctx context.Context, r WalletbetJSONRequestBody) pam.AddTransactionRequestMapper {
-	return func(ar pam.AmountRounder) (context.Context, *pam.AddTransactionRequest, error) {
+func promoBetTransactionMapper(ctx context.Context, r WalletbetJSONRequestBody) pam.AddTransactionRequestMapper {
+	return func(_ pam.AmountRounder) (context.Context, *pam.AddTransactionRequest, error) {
 		amt := toPamAmount(&r.Amount)
 		return ctx, &pam.AddTransactionRequest{
 			PlayerID: r.SupplierUser,
@@ -97,8 +97,8 @@ func (s *ProviderService) promoBetTransactionMapper(ctx context.Context, r Walle
 	}
 }
 
-func (s *ProviderService) winTransactionMapper(ctx context.Context, r WalletWinBody) pam.AddTransactionRequestMapper {
-	return func(ar pam.AmountRounder) (context.Context, *pam.AddTransactionRequest, error) {
+func winTransactionMapper(ctx context.Context, r WalletWinBody) pam.AddTransactionRequestMapper {
+	return func(_ pam.AmountRounder) (context.Context, *pam.AddTransactionRequest, error) {
 		amt := toPamAmount(&r.Amount)
 		return ctx, &pam.AddTransactionRequest{
 			PlayerID: r.SupplierUser,
@@ -122,8 +122,8 @@ func (s *ProviderService) winTransactionMapper(ctx context.Context, r WalletWinB
 		}, nil
 	}
 }
-func (s *ProviderService) promoWinTransactionMapper(ctx context.Context, r WalletWinBody) pam.AddTransactionRequestMapper {
-	return func(ar pam.AmountRounder) (context.Context, *pam.AddTransactionRequest, error) {
+func promoWinTransactionMapper(ctx context.Context, r WalletWinBody) pam.AddTransactionRequestMapper {
+	return func(_ pam.AmountRounder) (context.Context, *pam.AddTransactionRequest, error) {
 		amt := toPamAmount(&r.Amount)
 		return ctx, &pam.AddTransactionRequest{
 			PlayerID: r.SupplierUser,
@@ -148,8 +148,8 @@ func (s *ProviderService) promoWinTransactionMapper(ctx context.Context, r Walle
 	}
 }
 
-func (s *ProviderService) cancelTransactionMapper(ctx context.Context, r WalletrollbackJSONRequestBody, session *pam.Session, tt pam.TransactionType) pam.AddTransactionRequestMapper {
-	return func(ar pam.AmountRounder) (context.Context, *pam.AddTransactionRequest, error) {
+func cancelTransactionMapper(ctx context.Context, r WalletrollbackJSONRequestBody, session *pam.Session, tt pam.TransactionType) pam.AddTransactionRequestMapper {
+	return func(_ pam.AmountRounder) (context.Context, *pam.AddTransactionRequest, error) {
 		return ctx, &pam.AddTransactionRequest{
 			PlayerID: utils.OrZeroValue(r.User),
 			Params: pam.AddTransactionParams{
@@ -172,7 +172,7 @@ func (s *ProviderService) cancelTransactionMapper(ctx context.Context, r Walletr
 	}
 }
 
-func (s *ProviderService) getTransactionsMapper(ctx context.Context, r WalletWinBody) pam.GetTransactionsRequestMapper {
+func getTransactionsMapper(ctx context.Context, r WalletWinBody) pam.GetTransactionsRequestMapper {
 	return func() (context.Context, pam.GetTransactionsRequest, error) {
 		return ctx, pam.GetTransactionsRequest{
 			PlayerID: r.SupplierUser,
