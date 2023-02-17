bfa0553839c23bb7a1d85230e1c62729313752ff
Carl Bring <111354161+gnirb@users.noreply.github.com>
2023-02-17 13:27:32 +0100

    Return error when getRoundTransactions fails
    * this error will propagate back to Caleta, causing retries which is better than incorrect settlements

---
+++ provider/caleta/error_test.go
@@ -0,0 +1,49 @@
+package caleta
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/valkyrie-fnd/valkyrie/pam"
+	"github.com/valkyrie-fnd/valkyrie/rest"
+)
+
+func Test_getCErrorStatus(t *testing.T) {
+	tests := []struct {
+		name string
+		err  error
+		want Status
+	}{
+		{
+			"generic error",
+			errors.New("boom"),
+			RSERRORUNKNOWN,
+		},
+		{
+			"valkyrie timeout error",
+			pam.ValkyrieError{
+				ValkErrorCode: pam.ValkErrTimeout,
+			},
+			RSERRORTIMEOUT,
+		},
+		{
+			"valkyrie session expired error",
+			pam.ValkyrieError{
+				ValkErrorCode: pam.ValkErrOpSessionExpired,
+			},
+			RSERRORTOKENEXPIRED,
+		},
+		{
+			"http timeout error",
+			rest.TimeoutError,
+			RSERRORTIMEOUT,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, getCErrorStatus(tt.err), "getCErrorStatus(%v)", tt.err)
+		})
+	}
+}
