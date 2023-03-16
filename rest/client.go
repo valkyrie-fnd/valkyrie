6cc8295bfff42338cd6737b7c3bcedd90e701fe0
Gustav <103995817+gustavste@users.noreply.github.com>
2023-03-16 16:08:57 +0100

    fix rest package get request

--- rest/client.go
+++ rest/client.go
@@ -79,6 +79,9 @@ type requestContentFn func(*fasthttp.Request) error
 // Convenience method for reading response JSON
 func readJSON(target interface{}) responseParseFn {
 	return func(r *fasthttp.Response) error {
+		if r.Header.ContentLength() <= 0 {
+			return nil
+		}
 		err := json.Unmarshal(r.Body(), &target)
 		if err != nil {
 			return fmt.Errorf("json parsing error: %w", err)
@@ -110,6 +113,9 @@ func writeJSON(content interface{}) requestContentFn {
 // Convenience method for reading response XML
 func readXML(target interface{}) responseParseFn {
 	return func(r *fasthttp.Response) error {
+		if r.Header.ContentLength() <= 0 {
+			return nil
+		}
 		err := xml.Unmarshal(r.Body(), &target)
 		if err != nil {
 			return fmt.Errorf("xml parsing error: %w", err)
@@ -149,7 +155,7 @@ type HTTPRequest struct {
 // HTTPClientJSONInterface interface for client with JSON responses
 type HTTPClientJSONInterface interface {
 	GetJSON(ctx context.Context, req *HTTPRequest, resp any) error
-	Get(ctx context.Context, req *HTTPRequest, resp any) error
+	Get(ctx context.Context, req *HTTPRequest, resp *[]byte) error
 	PostJSON(ctx context.Context, req *HTTPRequest, resp any) error
 	PutJSON(ctx context.Context, req *HTTPRequest, resp any) error
 }
@@ -167,9 +173,9 @@ func (c *Client) GetJSON(ctx context.Context, req *HTTPRequest, resp any) error 
 }
 
 // Get Issue GET request with expected response body set to resp
-func (c *Client) Get(ctx context.Context, req *HTTPRequest, resp any) error {
+func (c *Client) Get(ctx context.Context, req *HTTPRequest, resp *[]byte) error {
 	return c.get(ctx, req.URL, func(r *fasthttp.Response) error {
-		resp = r.Body()
+		*resp = r.Body()
 		return nil
 	}, req.Headers, req.Query)
 }
@@ -285,9 +291,7 @@ func (c *Client) handle(
 func handleResponse(statusCode int, resp *fasthttp.Response, parseFn responseParseFn) error {
 	switch statusCode {
 	case http.StatusOK:
-		if resp.Header.ContentLength() > 0 {
-			return parseFn(resp)
-		}
+		return parseFn(resp)
 	case http.StatusCreated:
 		if resp.Header.ContentLength() > 0 {
 			return parseFn(resp)
